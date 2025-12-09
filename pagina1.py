e8800a6a537e44d7ff4f210f731b0d72ca1ca5e2
Joaquin-726 <jsalgado2024@udec.cl>
2025-12-08 21:26:19 -0300

    Fix formatting of 'Inicio' label in pagina1.py

--- pagina1.py
+++ pagina1.py
@@ -83,7 +83,8 @@ with tab_proposito:
         <div style="font-size:16px; line-height:1.6;">
 
         
-        <b>Inicio:</b> Evaluación del estudiante.<br><br>
+        <b>
+        Inicio:</b> Evaluación del estudiante.<br><br>
 
         <b>Condiciones críticas:</b>
         Si se cumple al menos una, se genera <b>ALERTA ALTA</b>.<br><br>
